f45b08a7a621c9f5137572ca32b2d2270cc956dc
AlbertoVC <37180792@uaz.edu.mx>
2024-02-07 19:13:53 -0600

    FileSystem features implemented.

--- src/main/java/com/tutego/date4u/core/FileSystem.java
+++ src/main/java/com/tutego/date4u/core/FileSystem.java
@@ -1,4 +1,43 @@
 package com.tutego.date4u.core;
 
+import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Component
 public class FileSystem {
+    private final Path root = Paths.get(System.getProperty("user.home")).resolve("fs");
+
+    public FileSystem() {
+        try{
+            if(!Files.isDirectory(root)) Files.createDirectories(root);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public long getFreeDiskSpace() {
+        return root.toFile().getFreeSpace();
+    }
+
+    public byte[] load( String filename ) {
+        try {
+            return Files.readAllBytes( root.resolve( filename ) );
+        }
+        catch ( Exception e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    public void store( String filename, byte[] data ) {
+        try {
+            Files.write( root.resolve( filename ), data );
+        }
+        catch ( Exception e ) {
+            throw new RuntimeException( e );
+        }
+    }
 }
