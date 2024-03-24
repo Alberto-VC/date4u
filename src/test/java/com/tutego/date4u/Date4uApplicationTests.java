beb34b96071815513c4dd5d297a33e38bdc3be79
Alberto-VC <37180792@uaz.edu.mx>
2024-03-24 17:38:16 -0600

    Shell disabled and test.properties added

--- src/test/java/com/tutego/date4u/Date4uApplicationTests.java
+++ src/test/java/com/tutego/date4u/Date4uApplicationTests.java
@@ -1,13 +1,26 @@
 package com.tutego.date4u;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:test.properties",
+                    properties = "spring.shell.interactive.enabled=false")
 class Date4uApplicationTests {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Value("${date4u.filesystem.minimum-free-disk-space}")
+    private Long minimumFreeDiskSpace;
 
     @Test
     void contextLoads() {
+        log.info( "minimumFreeDiskSpace: {}", minimumFreeDiskSpace );
     }
 
 }
