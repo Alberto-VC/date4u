3073556fe1702b8409de9271e41b8282302eb1cc
Alberto-VC <37180792@uaz.edu.mx>
2024-03-28 17:17:27 -0600

    Validations added to photo

--- src/main/java/com/tutego/date4u/core/photo/Photo.java
+++ src/main/java/com/tutego/date4u/core/photo/Photo.java
@@ -1,11 +1,43 @@
 package com.tutego.date4u.core.photo;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+
 import java.time.LocalDateTime;
 
 public class Photo {
+
     public Long id;
-    public Long profile;
+    @Min(1)
+    public long profile;
+    @NotNull
+    @Pattern(regexp = "[\\w_-]{1,200}")
     public String name;
     public boolean isProfilePhoto;
+    @NotNull
+    @Past
     public LocalDateTime created;
+
+    public Photo(){}
+
+    public Photo(Long id, Long profile, String name, boolean isProfilePhoto, LocalDateTime created){
+        this.id = id;
+        this.profile = profile;
+        this.name = name;
+        this.isProfilePhoto = isProfilePhoto;
+        this.created = created;
+    }
+
+    @Override
+    public String toString(){
+        return "Photo{" +
+                "id=" + id +
+                ", profile=" + profile +
+                ", name='" + name + '\'' +
+                ", isProfilePhoto=" + isProfilePhoto +
+                ", created=" + created +
+                '}';
+    }
 }
